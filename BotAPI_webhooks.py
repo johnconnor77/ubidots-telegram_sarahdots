cc171a8bab1fbdd341ab0f55e85efcd6a9834cd3
johnconnor77 <jfcalleh@unal.edu.co>
2020-09-03 05:40:04 -0500

    Modified for being runned by procfile

--- BotAPI_webhooks.py
+++ BotAPI_webhooks.py
@@ -167,6 +167,5 @@ async def update_bot(form_update: FormUpdate):
 async def delete_bot():
     pass
 
-
-if __name__ == "__main__":
-    uvicorn.run("BotAPI_webhooks:app", host="127.0.0.1", port=5000, log_level="info")
+#if __name__ == "__main__":
+#   uvicorn.run("BotAPI_webhooks:app", host="127.0.0.1", port=5000, log_level="info")
