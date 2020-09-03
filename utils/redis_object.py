d40ca086360a3dab7bcb4f30d0d3b1254f81ee38
johnconnor77 <jfcalleh@unal.edu.co>
2020-09-03 14:53:12 -0500

    Update for making database located at other modules

--- utils/redis_object.py
+++ utils/redis_object.py
@@ -1,13 +1,16 @@
 import aioredis
 from utils.constants import TEST, TEST_REDIS_URL, REDIS_URL, REDIS_DB, REDIS_PWD
 
-redis = None
+# redis = None
 
 
 async def redis_connection():
     if TEST:
         redis_db = await aioredis.create_redis_pool(TEST_REDIS_URL)
     else:
-        redis_db = await aioredis.create_redis_pool(REDIS_URL,
-                                                    db=REDIS_DB, password=REDIS_PWD)
+        redis_db = await aioredis.create_redis_pool(REDIS_URL, db=REDIS_DB, password=REDIS_PWD)
     return redis_db
+
+
+async def redis():
+    return None
